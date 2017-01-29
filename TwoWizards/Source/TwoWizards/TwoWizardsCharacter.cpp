2827daf249af534849c17f03c089cd6bef60bd72
Anurag Papineni <anuragpapineni@gmail.com>
2017-01-29 17:18:04 -0600

    firing is back

--- TwoWizards/Source/TwoWizards/TwoWizardsCharacter.cpp
+++ TwoWizards/Source/TwoWizards/TwoWizardsCharacter.cpp
@@ -65,6 +65,7 @@ void ATwoWizardsCharacter::BeginPlay()
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 
+
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -74,6 +75,8 @@ void ATwoWizardsCharacter::SetupPlayerInputComponent(class UInputComponent* Play
 {
 	// set up gameplay key bindings
 	check(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &ATwoWizardsCharacter::OnFire);
 
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
